fd361f9253d5aa67ebf78f25d19b2b40775a0609
mrkroms <spklgd@gmail.com>
2024-01-15 15:30:02 +0200

    use class components in employees-add-form

--- my-app/src/components/employees-add-form/employees-add-form.js
+++ my-app/src/components/employees-add-form/employees-add-form.js
@@ -1,26 +1,75 @@
+import { Component } from "react";
 import "./employees-add-form.css";
-const EmployeesAddForm = () => {
-  return (
-    <div className="app-add-form">
-      <h3>Добавьте нового сотрудника</h3>
-      <form className="add-form d-flex">
-        <input
-          type="text"
-          className="form-control new-post-label"
-          placeholder="Как его зовут?"
-        />
-        <input
-          type="number"
-          className="form-control new-post-label"
-          placeholder="З/П в $?"
-        />
 
-        <button type="submit" className="btn btn-outline-light">
-          Добавить
-        </button>
-      </form>
-    </div>
-  );
-};
+class EmployeesAddForm extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      nameEmployee: "",
+      salary: "",
+    };
+  }
+
+  onChangeValue = (e) => {
+    this.setState({ [e.target.name]: e.target.value });
+  };
+
+  render() {
+    const { nameEmployee, salary } = this.state;
+    return (
+      <div className="app-add-form">
+        <h3>Добавьте нового сотрудника</h3>
+        <form className="add-form d-flex">
+          <input
+            type="text"
+            className="form-control new-post-label"
+            placeholder="Как его зовут?"
+            name="nameEmployee"
+            value={nameEmployee}
+            onChange={this.onChangeValue}
+          />
+          <input
+            type="number"
+            className="form-control new-post-label"
+            placeholder="З/П в $?"
+            name="salary"
+            value={salary}
+            onChange={this.onChangeValue}
+          />
+
+          <button type="submit" className="btn btn-outline-light">
+            Добавить
+          </button>
+        </form>
+      </div>
+    );
+  }
+}
 
 export default EmployeesAddForm;
+
+// const EmployeesAddForm = () => {
+//   return (
+//     <div className="app-add-form">
+//       <h3>Добавьте нового сотрудника</h3>
+//       <form className="add-form d-flex">
+//         <input
+//           type="text"
+//           className="form-control new-post-label"
+//           placeholder="Как его зовут?"
+//         />
+//         <input
+//           type="number"
+//           className="form-control new-post-label"
+//           placeholder="З/П в $?"
+//         />
+
+//         <button type="submit" className="btn btn-outline-light">
+//           Добавить
+//         </button>
+//       </form>
+//     </div>
+//   );
+// };
+
+// export default EmployeesAddForm;
